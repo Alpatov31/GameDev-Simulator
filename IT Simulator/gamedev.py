c5a9ddf8e010c79c41d8a6c4d3ef4a263773c31c
Alpatov31 <alpatov31@yandex.ru>
2021-10-28 15:27:05 +0300

    Made a window and ignored unnecessary files

---
+++ IT Simulator/gamedev.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+
+root['bg'] = '#808080'
+root.title('Gamedev Simulator')
+root.geometry('1200x800')
+
+root.resizable(width=False, height=False)
+
+root.mainloop()
